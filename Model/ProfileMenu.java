014f5d85ceb5d66ec63735e63cbbedf2468dac66
parnia1384 <168190104+parnia1384@users.noreply.github.com>
2024-07-09 18:29:06 +0330

    ProfileMenu.java

--- Model/ProfileMenu.java
+++ Model/ProfileMenu.java
@@ -1,92 +1,47 @@
 package Model;
 
+import javafx.scene.control.Label;
+import util.Constants;
+
 import java.util.Scanner;
 import java.util.regex.Matcher;
 public class ProfileMenu extends RegistryMenu{
-    public void myProfile(Scanner scanner, User loggedInUser){
-        String command;
-        Matcher matcher;
-        String changeUsername = "Profile change -u (\\S+)";
-        String changeNickname = "Profile change -n (\\S+)";
-        String changeEmail = "Profile change -e (\\S+)";
-        String changePassword = "Profile change password -o (\\S+) -n (\\S+)";
-        while(true){
-            command = scanner.nextLine();
-            if(command.equals("Exit profile menu")){
-                System.out.println("returned to main menu successfully.");
-                break;
-            }
-            else if(command.equals("Show profile")) showProfile(loggedInUser);
-            else if(command.matches(changeUsername)){
-                matcher = getCommandMatcher(command, changeUsername);
-                changeUsername(matcher, loggedInUser);
-            }
-            else if(command.matches(changeNickname)){
-                matcher = getCommandMatcher(command, changeNickname);
-                changeNickname(matcher, loggedInUser);
-            }
-            else if(command.matches(changeEmail)){
-                matcher = getCommandMatcher(command, changeEmail);
-                changeEmail(matcher, loggedInUser);
-            }
-            else if(command.matches(changePassword)){
-                matcher = getCommandMatcher(command, changePassword);
-                changePassword(matcher, loggedInUser);
-            }
-            else if(command.equals("Show my deck")){
-                loggedInUser.showDeck();
-            }
-            else if(command.equals("Show my history")){
-                showMyHistory(scanner, loggedInUser);
-            }
-            else if(command.equals("command prompt")) System.out.println(output.profileMenuManual);
-            else System.out.println("invalid input!");
+    public void changeUsername(String newUsername, Label label){
+        if(!isUsernameCorrect(newUsername)){
+            label.setText(output.wrongUsernameFormat);
+        }
+        else if(!isUsernameNew(newUsername)){
+            label.setText(output.duplicateUsername);
+        }
+        else{
+            Constants.loggedInUser.changeUsername(newUsername);
+            label.setText("username changed successfully!");
         }
     }
-    private void showProfile(User user){
-        System.out.println("Username: " + user.getUsername() + "\nPassword: " + user.getPassword() + "\nEmail: " + user.getEmail() + "\nNickname: " + user.getNickname()
-                + "\nLevel: " + user.getLevel() + "\nExp: " + user.getExp() + "\nHp: " + user.getHp() + "\nCoin: " + user.getCoin() + "\nScore: " + user.getScore());
-    }
-    private void changeUsername(Matcher matcher, User user){
-        if(!matcher.matches()) System.out.println("invalid input!");
+    public void changeNickname(String newNickname, Label label){
+        if(!isUsernameCorrect(newNickname)) label.setText("Please try again.");
         else{
-            String newUsername = matcher.group(1);
-            if(!isUsernameCorrect(newUsername)) System.out.println(output.wrongUsernameFormat);
-            else if(!isUsernameNew(newUsername)) System.out.println(output.duplicateUsername);
-            else user.changeUsername(newUsername);
+            Constants.loggedInUser.changeNickname(newNickname);
+            label.setText("Nickname changed successfully!");
         }
     }
-    private void changeNickname(Matcher matcher, User user){
-        if(!matcher.matches()) System.out.println("invalid input!");
+    public void changeEmail(String newEmail, Label label){
+        if(!isEmailCorrect(newEmail)) label.setText(output.wrongEmailFormat);
         else{
-            String newNickname = matcher.group(1);
-            if(!isUsernameCorrect(newNickname)) System.out.println("Please try again.");
-            else user.changeNickname(newNickname);
+            Constants.loggedInUser.changeEmail(newEmail);
+            label.setText("Email changed successfully!");
         }
     }
-    private void changeEmail(Matcher matcher, User user){
-        if(!matcher.matches()) System.out.println("invalid input!");
-        else{
-            String newEmail = matcher.group(1);
-            if(!isEmailCorrect(newEmail)) System.out.println(output.wrongEmailFormat);
-            else user.changeEmail(newEmail);
-        }
-    }
-    private void changePassword(Matcher matcher, User user){
-        if(!matcher.matches()) System.out.println("invalid input!");
-        else{
-            String oldPass = matcher.group(1);
-            String newPass = matcher.group(2);
-            if(!user.getPassword().equals(oldPass)) System.out.println(output.wrongPasswordEnteredForChange);
-            else if(newPass.length() < 8 || newPass.length() > 32) System.out.println(output.passwordLengthFault);
-            else if(!doesPasswordContainsLowerCase(newPass) || !doesPasswordContainUpperCase(newPass))
-                System.out.println(output.passwordDoesNotContainUpperOrLowerCase);
-            else if(!doesPasswordContainNumber(newPass)) System.out.println(output.passwordDoesNotContainNumber);
-            else if(!passwordContainsCharacter(newPass)) System.out.println(output.passwordDoesNotContainCharacters);
-            else{
-                user.changePassword(newPass);
-                System.out.println(output.passwordChanged);
-            }
+    public void changePassword(String oldPass, String newPass, Label label){
+        if(oldPass.isEmpty() || newPass.isEmpty()) label.setText(output.emptyField);
+        else if(!Constants.loggedInUser.getPassword().equals(oldPass)) label.setText(output.wrongPasswordEnteredForChange);
+        else if(newPass.length() < 8 || newPass.length() > 32) label.setText(output.passwordLengthFault);
+        else if(!doesPasswordContainsLowerCase(newPass) || !doesPasswordContainUpperCase(newPass)) label.setText(output.passwordDoesNotContainUpperOrLowerCase);
+        else if(!doesPasswordContainNumber(newPass)) label.setText(output.passwordDoesNotContainNumber);
+        else if(!passwordContainsCharacter(newPass)) label.setText(output.passwordDoesNotContainCharacters);
+        else {
+            Constants.loggedInUser.changePassword(newPass);
+            label.setText(output.passwordChanged);
         }
     }
     public void showMyHistory(Scanner scanner, User user){
