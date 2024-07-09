ed55ec1da87f5bc283853ec71f523e7ab2a094af
parnia1384 <168190104+parnia1384@users.noreply.github.com>
2024-07-09 22:28:55 +0330

    MainMenu.java

--- Controller/MainMenu.java
+++ Controller/MainMenu.java
@@ -1,5 +1,6 @@
 package Controller;
 
+import Model.User;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -7,8 +8,13 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
+import util.Constants;
 
 
 import java.io.IOException;
@@ -19,6 +25,63 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class MainMenu {
+    @FXML
+    Button logout;
+    @FXML
+    Button returnToMainMenu;
+    @FXML
+    TextField username;
+    @FXML
+    PasswordField password;
+    @FXML
+    Button submit;
+    @FXML
+    Label label;
+    @FXML
+    private void handleReturnToMainMenuButtonAction(ActionEvent event){
+        try{
+            switchScene(event, "MainPage.fxml");
+        }
+        catch (IOException e){
+            System.out.println(e);
+        }
+    }
+    @FXML
+    private void handleSubmitButtonAction(ActionEvent event){
+        String username = this.username.getText();
+        String password = this.password.getText();
+        if(username.isEmpty() || password.isEmpty()) label.setText(Constants.outputs.emptyField);
+        else if (username.equals(Constants.loggedInUser.getUsername())) {
+            label.setText("Choose another username!");
+        }
+        else if(Constants.registryMenu.findUserByUsername(username) == null){
+            label.setText(Constants.outputs.usernameDoesNotExist);
+        }
+        else{
+            User user = Constants.registryMenu.findUserByUsername(username);
+            if(!user.getPassword().equals(password)) label.setText(Constants.outputs.wrongPasswordEntered);
+            else{
+                Constants.secondUser = user;
+                label.setText("OK!");
+                try{
+                    switchScene(event, "GamePage.fxml");
+                }
+                catch (IOException e){
+                    System.out.println(e);
+                }
+            }
+        }
+    }
+    @FXML
+    private void logout(ActionEvent event){
+        try{
+            Constants.writeInformationInFile();
+            switchScene(event, "StarterPage.fxml");
+        }
+        catch (IOException e){
+            System.out.println(e);
+        }
+    }
     @FXML
     public void back(ActionEvent event) {
         try {
@@ -38,7 +101,7 @@ public class MainMenu {
     @FXML
     public void startGameBtn(ActionEvent event) {
         try {
-            switchScene(event, "GamePage.fxml");
+            switchScene(event, "guestLogin.fxml");
         } catch (IOException e) {
             e.printStackTrace();
         }
