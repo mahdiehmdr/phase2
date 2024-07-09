ece14288f5e96bb5c61d81fe1adbfd252c55d9bd
parnia1384 <168190104+parnia1384@users.noreply.github.com>
2024-07-09 22:27:00 +0330

    HelloApplication.java

--- Controller/HelloApplication.java
+++ Controller/HelloApplication.java
@@ -4,21 +4,24 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import util.Constants;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("GamePage.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("StarterPage.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1200, 800);
-        stage.setTitle("Hello!");
+        stage.setTitle("Hogwarts war");
         stage.setScene(scene);
         stage.setResizable(true);
         stage.show();
     }
-
+    @Override
+    public void stop(){
+        Constants.writeInformationInFile();
+    }
     public static void main(String[] args) {
         launch();
     }
